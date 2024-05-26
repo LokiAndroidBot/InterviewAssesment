5b6913037a34e882537323ef3c04f54f829c56e4
Loki <logeshwaranwill@gmail.com>
2024-05-27 00:14:33 +0530

    Firebase Integration Code

--- app/src/main/java/com/indalph/interviewassessment/MainActivity.kt
+++ app/src/main/java/com/indalph/interviewassessment/MainActivity.kt
@@ -8,6 +8,7 @@ import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -34,32 +35,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.Logger
-import com.indalph.interviewassessment.parent.dataTypeSize
-import com.indalph.interviewassessment.parent.doAnyType
-import com.indalph.interviewassessment.parent.doBy
-import com.indalph.interviewassessment.parent.doDataClass
-import com.indalph.interviewassessment.parent.doDiffEquality
-import com.indalph.interviewassessment.parent.doDoubleBang
-import com.indalph.interviewassessment.parent.doElvis
-import com.indalph.interviewassessment.parent.doEnumClass
-import com.indalph.interviewassessment.parent.doExtensionString
-import com.indalph.interviewassessment.parent.doInit
-import com.indalph.interviewassessment.parent.doInnerClass
-import com.indalph.interviewassessment.parent.doIt
-import com.indalph.interviewassessment.parent.doLateInit
-import com.indalph.interviewassessment.parent.doLazy
-import com.indalph.interviewassessment.parent.doMutableAndImmutable
-import com.indalph.interviewassessment.parent.doMutableImmutableList
-import com.indalph.interviewassessment.parent.doNestedClass
-import com.indalph.interviewassessment.parent.doNullable
-import com.indalph.interviewassessment.parent.doPrivateVisibility
-import com.indalph.interviewassessment.parent.doProtectedVisibility
-import com.indalph.interviewassessment.parent.doPublicVisibility
-import com.indalph.interviewassessment.parent.doSafeCall
-import com.indalph.interviewassessment.parent.doVariableType
 import com.indalph.interviewassessment.ui.theme.InterviewAssessmentTheme
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
@@ -68,12 +45,13 @@ import kotlin.reflect.full.memberProperties
 
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG)
         enableEdgeToEdge()
         setContent {
-            val viewModel: MainViewModel = viewModel()
             val state by viewModel.state.collectAsState()
 
             LaunchedEffect(Unit) {
@@ -83,122 +61,50 @@ class MainActivity : ComponentActivity() {
             MainScreen(state, viewModel)
         }
     }
+
 }
 
 @Composable
-fun MainScreen(state: MainState, mainViewModel: MainViewModel) {
-    when (state) {
-        is MainState.Idle -> {
-            Text("Idle")
-        }
-
-        is MainState.Loading -> {
-            Column(
-                modifier = Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally
-            ) {
-                CircularProgressIndicator()
-            }
-
-        }
-
-        is MainState.Data -> {
-            ExpandableList(state.response, mainViewModel)
-        }
-
-        is MainState.Error -> {
-            Text("Error: ${state.error}")
-        }
-
-        is MainState.CHILD -> {
-            Column(
-                modifier = Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally
-            ) {
-                ExpandableList(state.response)
-            }
-
-        }
+fun MainScreen(state: MainState, mainViewModel: MainViewModel) = when (state) {
+    is MainState.Idle -> {
+        Text("Idle")
     }
-}
 
+    is MainState.Loading -> {
+        Column(
+            modifier = Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator()
+        }
 
-enum class ParentList {
-    BASIC, VARIABLES, CLASS, FUNCTION, STRING,
-}
+    }
 
-fun getParent(): List<String> = listOf(
-    ParentList.BASIC.name,
-    ParentList.VARIABLES.name,
-    ParentList.CLASS.name,
-    ParentList.FUNCTION.name,
-    ParentList.STRING.name
-)
+    is MainState.Data -> {
+        ExpandableList(state.response, mainViewModel)
+    }
 
-data class Data(val parent: String, val message: String, val function: () -> String)
+    is MainState.Error -> {
+        Text("Error: ${state.error}")
+    }
 
-fun getBasicList() = listOf(
-    Data(ParentList.BASIC.name, "Visibility (Public)", ::doPublicVisibility),
-    Data(ParentList.BASIC.name, "Visibility (Private)", ::doPrivateVisibility),
-    Data(ParentList.BASIC.name, "Visibility (Protected)", ::doProtectedVisibility),
-    Data(ParentList.BASIC.name, "Diff == & ===", ::doDiffEquality),
-    Data(ParentList.BASIC.name, "Init Block", ::doInit)
-)
+    is MainState.CHILD -> {
+        Column(
+            modifier = Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally
+        ) {
+            ExpandableList(state.response)
+        }
 
-
-fun getVariablesList() = listOf(
-    Data(ParentList.VARIABLES.name, "Data Type Size", ::dataTypeSize),
-    Data(ParentList.VARIABLES.name, "Variable Type", ::doVariableType),
-    Data(ParentList.VARIABLES.name, "Lazy (by lazy{})", ::doLazy),
-    Data(ParentList.VARIABLES.name, "Late Init", ::doLateInit),
-    Data(ParentList.VARIABLES.name, "Nullable", ::doNullable),
-    Data(ParentList.VARIABLES.name, "Safe Call (?.)", ::doSafeCall),
-    Data(ParentList.VARIABLES.name, "Elvis Operator (?:)", ::doElvis),
-    Data(ParentList.VARIABLES.name, "Double Bang (!!)", ::doDoubleBang),
-    Data(ParentList.VARIABLES.name, "by keyword", ::doBy),
-    Data(ParentList.VARIABLES.name, "it keyword", ::doIt),
-    Data(ParentList.VARIABLES.name, "Mutable & Immutable", ::doMutableAndImmutable),
-    Data(ParentList.VARIABLES.name, "MutableList & ImmutableList", ::doMutableImmutableList),
-    Data(ParentList.VARIABLES.name, "Any (Variables & Member Function", ::doAnyType),
-)
-
-
-fun getClassList() = listOf(
-    Data(ParentList.CLASS.name, "Data Class", ::doDataClass),
-    Data(ParentList.CLASS.name, "Enum Class", ::doEnumClass),
-    Data(ParentList.CLASS.name, "Nested Class", ::doNestedClass),
-    Data(ParentList.CLASS.name, "Inner Class", ::doInnerClass)
-)
-
-fun getStringList() = listOf(
-    Data(
-        ParentList.STRING.name, "DoLazy", ::dataTypeSize
-    )
-)
-
-fun getFunctionList() = listOf(
-    Data(ParentList.FUNCTION.name, "Extension (String)", ::doExtensionString)
-)
-
-
-fun child(): List<Data> {
-    val mutableChild = mutableListOf<Data>()
-    mutableChild.addAll(getVariablesList())
-    mutableChild.addAll(getClassList())
-    mutableChild.addAll(getStringList())
-    mutableChild.addAll(getFunctionList())
-    mutableChild.addAll(getBasicList())
-
-    return mutableChild
-
+    }
 }
 
 @Composable
 fun ExpandableList(response: List<String>) {
     var selectedURL by remember { mutableStateOf("") }
-    Column(modifier = Modifier) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Column(
             modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .weight(1f)
+
+                .weight(0.3f)
                 .padding(top = 20.dp, bottom = 20.dp)
         ) {
             response.forEach { parent ->
@@ -207,17 +113,14 @@ fun ExpandableList(response: List<String>) {
                     .clickable {
                         selectedURL = parent
                     }) {
-                    Text(text = parent, fontSize = 20.sp, modifier = Modifier
-                        .clickable {
-                        }
-                        .padding(8.dp))
+                    Text(text = parent, fontSize = 20.sp, modifier = Modifier.padding(8.dp))
                 }
             }
 
         }
         Box(
             modifier = Modifier
-                .weight(1f)
+                .weight(0.7f)
                 .padding(top = 20.dp, bottom = 20.dp)
 
         ) {
@@ -325,6 +228,7 @@ fun WebViewExample(url: String) {
         it.loadUrl(url)
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
